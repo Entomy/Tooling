70740267c61036b2e90f6a935b7d13c5683bb847
Patrick Kelly <p-kell@live.com>
2022-12-05 06:32:46 -0500

    Missed a file

---
+++ Tests/LcmTests.cs
@@ -0,0 +1,11 @@
+﻿namespace System;
+
+public partial class NumbersomeExtensionsTests {
+	[Theory]
+	[InlineData(1.0, 1.0, 1.0)]
+	[InlineData(2.0, 2.0, 2.0)]
+	[InlineData(2.0, 4.0, 4.0)]
+	[InlineData(20.0, 4.0, 20.0)]
+	[InlineData(20.0, 15.0, 60.0)]
+	public void Lcm(Double first, Double second, Double expected) => Assert.Equal(expected, first.Lcm(second));
+}
